baed3c16b31ab1287163d65665e2fceab5ca6a7f
SPERASOFT\evgeniy.kolesnikov <EK>
2022-04-15 01:15:10 -0700

    init + swagger.yaml

---
+++ src/main/java/com/sperasoft/passportapi/PassportApiApplication.java
@@ -0,0 +1,13 @@
+package com.sperasoft.passportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassportApiApplication.class, args);
+    }
+
+}
