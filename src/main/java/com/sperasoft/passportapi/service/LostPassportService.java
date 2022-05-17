ade985629f1f7b4963ec9c783b6b725920262e1b
evgenkolesman <glavstroi_e@mail.ru>
2022-05-17 10:44:44 +0300

    Fix interfaces, endpoints

--- src/main/java/com/sperasoft/passportapi/service/LostPassportService.java
+++
@@ -1,7 +0,0 @@
-package com.sperasoft.passportapi.service;
-
-import com.sperasoft.passportapi.model.Description;
-
-public interface LostPassportService {
-    boolean deactivatePassport(String personId, String id, boolean active, Description description);
-}
