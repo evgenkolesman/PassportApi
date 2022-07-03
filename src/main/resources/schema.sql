f4e2eaf079233a0f38126d96b09099bf8b3e1a2b
euhenios <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>
2022-07-03 17:09:42 +0300

    Second step adding postgres from docker-compose

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,19 @@
 -- DROP DATABASE PassportApi;
 -- CREATE DATABASE IF NOT EXISTS PassportApi;
-CREATE TABLE IF NOT EXISTS passportapi.public.Person(
+CREATE TABLE IF NOT EXISTS passportapi1.public.Person(
     id varchar(50) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     birthday DATE NOT NULL,
     birthdayCountry VARCHAR(5) NOT NULL
 );
-CREATE TABLE IF NOT EXISTS passportapi.public.Passport(
+CREATE TABLE IF NOT EXISTS passportapi1.public.Passport(
     id VARCHAR(50) PRIMARY KEY,
     number VARCHAR(12) NOT NULL ,
     givenDate DATE NOT NULL,
     departmentCode VARCHAR(5) NOT NULL,
     active BOOLEAN NOT NULL,
     description TEXT,
-    Person_id VARCHAR REFERENCES passportapi.public.Person(id)
+    Person_id VARCHAR REFERENCES passportapi1.public.Person(id)
     );
 
 -- ALTER TABLE passportapi.public.Passport ADD FOREIGN KEY (Person_id)
