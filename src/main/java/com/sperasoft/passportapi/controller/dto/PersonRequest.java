7a54643c1c3b7d92093e359f7b906ddd072cdf31
Evgeniy Kolesnikov <glavstroi_e@mail.ru>
2022-07-20 20:10:12 +0300

    First step testing

--- src/main/java/com/sperasoft/passportapi/controller/dto/PersonRequest.java
+++ src/main/java/com/sperasoft/passportapi/controller/dto/PersonRequest.java
@@ -13,15 +13,15 @@ import static org.springframework.format.annotation.DateTimeFormat.ISO;
 @Data
 public class PersonRequest {
 
-    @NotBlank(message = "Name field should be filled")
-    @Size(min = 2, message = "name must be minimum 2 characters long")
+    @NotNull(message = "Invalid data: Name field should be filled")
+    @Size(min = 2, message = "Invalid data: Name must be minimum 2 characters long")
     private final String name;
 
     @DateTimeFormat(iso = ISO.DATE)
-    @NotNull(message = "Given Date field shouldn`t be empty")
+    @NotNull(message = "Invalid data: Given Date field shouldn`t be empty")
     private final LocalDate birthday;
 
-    @NotBlank(message = "BirthdayCountry field should be filled")
-    @Size(min = 2, max = 2, message = "Birthday country should be formatted like ISO CODE (2 characters)")
+    @NotNull(message = "Invalid data: BirthdayCountry field should be filled")
+    @Size(min = 2, max = 2, message = "Invalid data: Birthday country should be formatted like ISO CODE (2 characters)")
     private final String birthdayCountry;
 }
