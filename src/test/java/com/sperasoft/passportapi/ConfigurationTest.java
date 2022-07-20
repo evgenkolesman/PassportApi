db89d0ee0517e9685dc3113432410560d5018f4e
Evgeniy Kolesnikov <glavstroi_e@mail.ru>
2022-07-20 15:04:17 +0300

    Experiment with skipping tests

--- src/test/java/com/sperasoft/passportapi/ConfigurationTest.java
+++ src/test/java/com/sperasoft/passportapi/ConfigurationTest.java
@@ -1,16 +1,8 @@
 package com.sperasoft.passportapi;
 
-import com.sperasoft.passportapi.controller.dto.PassportResponse;
-import com.sperasoft.passportapi.model.Passport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.function.BiPredicate;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Configuration
 public class ConfigurationTest {
