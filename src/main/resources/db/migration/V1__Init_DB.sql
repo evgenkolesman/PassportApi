98d3521883aadbfd6cd4fb429e307a8f208d4960
Evgeniy Kolesnikov <glavstroi_e@mail.ru>
2022-08-05 17:03:01 +0300

    AbstractIntegrationClass will be needed for test containers

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS passportapi1.public.Person
+CREATE TABLE IF NOT EXISTS Person
 (
     id              varchar(50) PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
     birthday        DATE         NOT NULL,
     birthdayCountry VARCHAR(2)   NOT NULL
 );
-CREATE TABLE IF NOT EXISTS passportapi1.public.Passport
+CREATE TABLE IF NOT EXISTS Passport
 (
     id             VARCHAR(50) PRIMARY KEY,
     number         VARCHAR(12) NOT NULL,
